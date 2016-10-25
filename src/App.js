4820dcbb518356f28614f712c1fa5373f7f57339
Anna Rogers <2muddybarefeet@gmail.com>
2016-10-24 17:23:01 -0700

    sort working and arrows added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 const data = [
   {ItemName: "banana",NumberPounds: "2",PricePerPound:"0.50",ExpirationDate:"2014-11-06"},
   {ItemName: "kitkat",NumberPounds: "9",PricePerPound:"1.85",ExpirationDate:"2014-01-16"},
-  {ItemName: "almond",NumberPounds: "5",PricePerPound:"7.55",ExpirationDate:"2014-10-03"},
-  {ItemName: "curry",NumberPounds: "39",PricePerPound:"5.85",ExpirationDate:"2014-05-30"}
+  {ItemName: "almond",NumberPounds: "51",PricePerPound:"7.55",ExpirationDate:"2014-10-03"},
+  {ItemName: "curry",NumberPounds: "39",PricePerPound:"53.85",ExpirationDate:"2014-05-30"}
 ]
 
 class App extends Component {
@@ -14,20 +14,36 @@ class App extends Component {
     super(props);
     this.state = {
       tableData: data,
-      sortCol: null,
-      sortDirection: null
+      sortCol: "ItemName",
+      sortDirection: "asc",
+      sortArrows: {}
    }
   }
 
+  componentWillMount() {
+    var fields = Object.keys(this.state.tableData[0]);
+    var initalArrows = {};
+    for (var i = 0; i < fields.length; i++) {
+      initalArrows[fields[i]] = "\\/";
+    }
+
+    this.setState({
+      sortArrows: initalArrows
+    });
+  }
+
   handleClickHeader (colName) {
-    console.log('col name', colName);
     //if sort direction set then set it
     //if the sort col is the same as current then swap else asc
     var currSortDir = this.state.sortDirection === "asc" ? "desc" : "asc";
+
+    var arrowDir = Object.assign({}, this.state.sortArrows);
+    arrowDir[colName] = arrowDir[colName] === "\\/" ? "/\\" : "\\/";
     
     this.setState({
       sortCol: colName,
-      sortDirection: currSortDir
+      sortDirection: currSortDir,
+      sortArrows: arrowDir
     })
 
   }
@@ -35,22 +51,36 @@ class App extends Component {
   render() {
 
     var tableHeaders = Object.keys(this.state.tableData[0]).map( (item, index) => {
-      return <th key={index} onClick={this.handleClickHeader.bind(this,item)}>{item}</th>
+      return <th key={index} onClick={this.handleClickHeader.bind(this,item)}>{item}<span>{this.state.sortArrows[item]}</span></th>
     })
 
     var sortedData = this.state.tableData.sort( (a,b) => {
-      
-
 
       if (a[this.state.sortCol]) {
-        if (a[this.state.sortCol] === b[this.state.sortCol]) return 0;
-        if (a[this.state.sortCol] < b[this.state.sortCol]) return -1;
-        if (a[this.state.sortCol] > b[this.state.sortCol]) return 1;
+
+        if ( !isNaN(parseFloat(a[this.state.sortCol])) && !a[this.state.sortCol].match(/-/gi) ) {
+          console.log('number sort');
+          if (this.state.sortDirection === "asc") return parseFloat(a[this.state.sortCol]) - parseFloat(b[this.state.sortCol]);
+          if (this.state.sortDirection === "desc") return parseFloat(b[this.state.sortCol]) - parseFloat(a[this.state.sortCol]);
+        } else if ( !isNaN(Date.parse(a[this.state.sortCol])) ) {
+          if (this.state.sortDirection === "asc") return Date.parse(a[this.state.sortCol]) - Date.parse(b[this.state.sortCol]);
+          if (this.state.sortDirection === "desc") return Date.parse(b[this.state.sortCol]) - Date.parse(a[this.state.sortCol]);
+        } else {
+          if (a[this.state.sortCol] === b[this.state.sortCol]) return 0;
+          if (this.state.sortDirection === "asc") {
+            if (a[this.state.sortCol] < b[this.state.sortCol]) return -1;
+            if (a[this.state.sortCol] > b[this.state.sortCol]) return 1;
+          } else {
+            if (a[this.state.sortCol] > b[this.state.sortCol]) return -1;
+            if (a[this.state.sortCol] < b[this.state.sortCol]) return 1;
+          }
+        }
+
       }
 
     });
 
-    var tableData = sortedData.map(function (datum, index) {
+    var tableData = sortedData.map( (datum, index) => {
       var row = [];
       for (var key in datum) {
         row.push(<td key={key}>{datum[key]}</td>)
